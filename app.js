018ad535aad64acd1c77e1b148e9f7526aa31160
m-alchemist <alkhamis@email.arizona.edu>
2017-04-25 18:33:01 +0300

    first commit

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 var express=require('express');
 
 var path=require('path');
+const LoginWithTwitter = require('login-with-twitter')
+
+const tw = new LoginWithTwitter({
+  consumerKey: 'unjlXpB0w32UNwQW7XRA43cW3',
+  consumerSecret: 'OjCqm9GSl83hMzlJLuVBtW5YIDFwpucXe1xgApaSz73pdNPZGz',
+  callbackUrl: 'https://malchemist-voting-app.herokuapp.com/twitter/callback'
+})
 var app=module.exports=express();
 
 var port = process.env.PORT || 3000;
 
-app.get('/twitter/', function (req, res) {
-  const LoginWithTwitter = require('login-with-twitter')
+app.get('/twitter', function (req, res) {
 
-  const tw = LoginWithTwitter({
-    consumerKey: 'unjlXpB0w32UNwQW7XRA43cW3',
-    consumerSecret: 'OjCqm9GSl83hMzlJLuVBtW5YIDFwpucXe1xgApaSz73pdNPZGz',
-    callbackUrl: 'https://malchemist-voting-app.herokuapp.com/twitter/callback'
-  })
 })
 app.get('/twitter/callback', function (req, res) {
-
+res.send('hello worlds');
 })
 app.use(express.static(__dirname +'/public'));
 // app.use(express.static(path.join(__dirname, 'style')));
