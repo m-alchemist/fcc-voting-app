b84f8a7d278b116502fe7ec2ab9b69e784a85b82
m-alchemist <alkhamis@email.arizona.edu>
2017-04-27 16:34:01 -0700

    config

--- config/auth.js
+++ config/auth.js
@@ -6,6 +6,6 @@ module.exports = {
 	'twitterAuth' : {
 		'consumerKey' 		: 'unjlXpB0w32UNwQW7XRA43cW3',
 		'consumerSecret' 	: 'OjCqm9GSl83hMzlJLuVBtW5YIDFwpucXe1xgApaSz73pdNPZGz',
-		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+		'callbackURL' 		: 'https://malchemist-voting-app.herokuapp.com/auth/twitter/callback'
 	}
 };
